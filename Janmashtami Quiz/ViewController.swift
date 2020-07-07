36ec1813cf14445f2b98c8b53c1c4f6f11e27e52
Vihaan Thakore <vihaan.thakore@hotmail.com>
2020-07-07 16:47:07 -0500

    Initial Commit

---
+++ Janmashtami Quiz/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Janmashtami Quiz
+//
+//  Created by Vihaan Thakore on 7/7/20.
+//  Copyright © 2020 Vihaan Thakore. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
